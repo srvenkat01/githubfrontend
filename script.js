c0933da063dc35f9abce7e64abe3bf0f553660fa
srvenkat01 <venkatesh.ece2001@gmail.com>
2024-08-02 20:38:54 +0530

    Interactive Calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayValue(val){
+    document.getElementById("display").value= document.getElementById("display").value+val;
+}
+function clearDisplay(){
+    document.getElementById("display").value="";
+}
+function calculate(){
+    var userinput=document.getElementById("display").value;
+    var result=eval(userinput);
+    document.getElementById("display").value=result;
+}
